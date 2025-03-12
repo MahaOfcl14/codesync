b84b54c550863cc24bf655699fc30a07ad1da2b0
MahaOfcl14 <itzmahaarun@gmail.com>
2025-03-12 12:40:45 +0530

    Implemented DB with clerk sucessfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-10">
-     {/*  <SignedOut>
-        <SignInButton />
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn> */}
+    <SignInButton>
+      <Button>Login</Button>
+    </SignInButton>
       home page
     </div>
   );
