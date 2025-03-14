64c0ec4d5484ba15a605636705c37966ccb276e3
MahaOfcl14 <itzmahaarun@gmail.com>
2025-03-14 20:56:13 +0530

    Interview Scheduled and the DB gets updated

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -2,9 +2,9 @@
 
 import LoaderUI from '@/components/LoaderUI';
 import { useUserRole } from '@/hooks/useUserRole';
-import { useRouter } from 'next/router';
 import React from 'react'
 import InterviewScheduleUI from './InterviewScheduleUI';
+import { useRouter } from 'next/navigation';
 
 function SchedulePage() {
   const router = useRouter();
