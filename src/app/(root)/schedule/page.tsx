f3363aa32065a0ca5a129ae64b7671c7fc142659
MahaOfcl14 <itzmahaarun@gmail.com>
2025-03-14 19:53:56 +0530

    Interview schedule page component created

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+
+import LoaderUI from '@/components/LoaderUI';
+import { useUserRole } from '@/hooks/useUserRole';
+import { useRouter } from 'next/router';
 import React from 'react'
+import InterviewScheduleUI from './InterviewScheduleUI';
 
 function SchedulePage() {
-  return (
-    <div>SchedulePage</div>
-  )
-}
+  const router = useRouter();
+  const { isInterviewer, isLoading } = useUserRole();
 
-export default SchedulePage
+  if (isLoading) return <LoaderUI />;
+  if (!isInterviewer) return router.push("/");
+
+  return <InterviewScheduleUI />;
+}
+export default SchedulePage;
